d522e4cb06a49196636a5350d946b786b490302b
Artur Petryk <petrikartur@gmail.com>
2021-04-11 19:35:26 +0300

    fixed some bugs with random user recommendation

--- auth_app/models.py
+++ auth_app/models.py
@@ -6,8 +6,8 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pic')
-    description = models.TextField(blank=True)
     full_name = models.CharField(max_length=250, blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- posts_app/views.py
+++ posts_app/views.py
@@ -18,32 +18,31 @@ class PostListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         following_list = Follow.objects.filter(follower=self.request.user)
         posts_obj = Post.objects.filter(author__in=following_list.values_list('following')).order_by('-date_posted')
-        print(f'WWWWWWWWWWWWWWWW: {posts_obj}, {len(posts_obj)}')
+
         following_post_number = 0
         for i in following_list:
             following_post_number += i.following.post.count()
 
-        # recommend random users if there is not many posts to show (or few followings)
-        if following_post_number < 5 or len(following_list) < 3:
-            recommended_users = set()
-            while len(recommended_users) < 3:
-                last_user = User.objects.count() - 1
-                appropriate_pseudo_user = False
+        # recommend random users if there are not many posts to show (or few followings)
+        if User.objects.all().count() >= 5:
+            if following_post_number < 5 or len(following_list) < 3:
+                recommended_users = set()
+                while len(recommended_users) < 3:
+                    last_user = User.objects.count() - 1
+                    appropriate_pseudo_user = False
 
-                while not appropriate_pseudo_user:
-                    r = random.randint(0, last_user)
-                    recommended_user = User.objects.all()[r]
+                    while not appropriate_pseudo_user:
+                        r = random.randint(0, last_user)
+                        recommended_user = User.objects.all()[r]
 
-                    # if already following recommended_user pick another random one
-                    if not following_list.filter(following=recommended_user)\
-                            and recommended_user != self.request.user:
-                        appropriate_pseudo_user = True
+                        # if already following recommended_user pick another random one
+                        if not following_list.filter(following=recommended_user)\
+                                and recommended_user != self.request.user:
+                            appropriate_pseudo_user = True
 
-                recommended_users.add(recommended_user)
-                posts_obj |= Post.objects.filter(author=recommended_user)[:5]
-                posts_obj.order_by('-date_posted')
-            print(f'FFFFFFFFFFFFFFF: {recommended_users}')
-            print(f'DDDDDDDDDDDDDDD: {posts_obj}, {len(posts_obj)}')
+                    recommended_users.add(recommended_user)
+                    posts_obj |= Post.objects.filter(author=recommended_user)[:5]
+                    posts_obj.order_by('-date_posted')
         return posts_obj
 
     def post(self, request, *args, **kwargs):
